13c347c18be91df70df14085597fa3251a87cadc
Wino Dev <dev@wino.app>
2025-12-30 23:56:06 +0100

    Remove .next folder from git - should only exist locally

--- .next 2/dev/static/chunks/src_app_page_tsx_e3a30e50._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_3cc289ed._.js",
-  "static/chunks/node_modules__pnpm_d4b9f000._.js"
-],
-    source: "dynamic"
-});
